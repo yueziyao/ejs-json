b418c48a7aefef3178d5a17e86bba184d46f0993
Alex <351599628@qq.com>
2018-08-23 16:37:36 +0800

    Create ejs-json.js

---
+++ ejs-json.js
@@ -0,0 +1,68 @@
+'use strict'
+
+var through = require('through2')
+var PluginError = require('plugin-error')
+var replaceExtension = require('replace-ext')
+var ejs = require('ejs')
+var Buffer = require('safe-buffer').Buffer
+var fs = require("fs")
+
+var ejsJson = function (data, options, settings) {
+    data = data || {}
+    options = options || {}
+    settings = settings || {}
+    let src = data || {}
+
+    return through.obj(function (file, enc, cb) {
+        let filename =  src.data +'/'+ RemoveChinese(file.relative)+'.json';
+
+        if(fs.existsSync(filename)){
+            data = require(filename)
+        }else {
+            data = {}
+        }
+
+        if (file.isNull()) {
+            this.push(file)
+            return cb()
+        }
+
+        if (file.isStream()) {
+            this.emit(
+                'error',
+                new PluginError('trp-ejs', 'Streaming not supported')
+            )
+        }
+
+        var fileData = Object.assign({}, data, file.data)
+        options.filename = file.path
+
+        try {
+            file.contents = new Buffer.from(
+                ejs.render(file.contents.toString(), fileData, options)
+            )
+
+            if (typeof settings.ext !== 'undefined') {
+                file.path = replaceExtension(file.path, settings.ext)
+            }
+        } catch (err) {
+            this.emit('error', new PluginError('trp-ejs', err.toString()))
+        }
+
+        this.push(file)
+        cb()
+    })
+}
+
+ejsJson.ejs = ejs
+
+module.exports = ejsJson
+
+function RemoveChinese(strValue) {
+    if(strValue!= null && strValue != ""){
+        var reg = /-[\u4e00-\u9fa5].*/g;
+        return strValue.replace(reg, "");
+    }
+    else
+        return "";
+}
